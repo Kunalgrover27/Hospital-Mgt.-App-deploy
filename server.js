86f5737c259c84efcd467ce928f52c2adff1e726
Kunalgrover27 <kunalgrover.2704@gmail.com>
2024-03-28 16:10:10 +0530

    for deployment

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const http = require('http');
+const { Server } = require("socket.io");
+const path = require('path')
+
+//dotenv conig
+dotenv.config();
+
+//mongodb connection
+connectDB();
+
+//rest obejct
+const app = express();
+const server= http.createServer(app);
+
+//middlewares
+app.use(express.json());
+app.use(moragan("dev"));
+
+//routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+
+const io = new Server(server, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
+
+
+//static files
+app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get("*",function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+
+//port
+const port = process.env.PORT || 8080;
+//listen port
+server.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
